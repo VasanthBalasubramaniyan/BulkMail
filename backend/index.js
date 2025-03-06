4273eae7ee4bc0e330b9c38d1a330bbc58ceee37
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-03-06 20:44:11 +0530

    Updated all the files and Project Finished

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,76 @@
-const express = require("express")
-const cors = require("cors")
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-// Install Nodemailer
+const express = require("express");
+const cors = require("cors");
 const nodemailer = require("nodemailer");
+const app = express();
+const mongoose = require("mongoose");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "vasanthniet123@gmail.com",
-    pass: "cjpw gdfx pxtj abjo",
-  },
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://vasanthniet123:123@cluster0.aqryx.mongodb.net/passkey?retryWrites=true&w=majority&appName=Cluster0")
+  .then(function () {
+    console.log("connected to db");
+  })
+  .catch(function () {
+    console.log("Failed");
+  });
+
+const credential = mongoose.model("credential", {}, "bulkmail");
+
+app.post("/sendmail", function (req, res) {
+  var msg = req.body.msg;
+  var emailList = req.body.emailList;
+  console.log(msg);
+
+  credential
+    .find()
+    .then(function (data) {
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: data[0].toJSON().user,
+          pass: data[0].toJSON().pass,
+        },
+      });
+      new Promise(async function (resolve, reject) {
+        try {
+          for (var i = 0; i < emailList.length; i++) {
+            await transporter.sendMail(
+              {
+                from: "vasanthniet123@gmail.com",
+                to: emailList[i],
+                subject: "A message from Bulk Mail App",
+                text: msg,
+              },
+              function (error, info) {
+                if (error) {
+                  console.log(error);
+                  res.send(false);
+                } else {
+                  console.log(info);
+                  res.send(true);
+                }
+              }
+            );
+          }
+          resolve("Success");
+        } catch (error) {
+          reject("Failed");
+        }
+      })
+        .then(function () {
+          res.send(true);
+        })
+        .catch(function () {
+          res.send(false);
+        });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
 
-
-
-app.post("/sendmail",function(req,res){
-
-    var msg = req.body.msg
-    console.log(msg)
-
-    transporter.sendMail(
-        {
-            from: "vasanthniet123@gmail.com",
-            to: "vasanth02.msg@gmail.com",
-            subject:"A message from Bulk Mail App",
-            text: msg
-        },
-        function(error,info){
-            if (error) {
-                console.log(error)
-                res.send(false)
-            } else {
-                console.log(info)
-                res.send(true)
-            }
-        }
-    )
-})
-
-
-app.listen(5000,function(){
-    console.log("Server started")
-})
+app.listen(5000, function () {
+  console.log("Server started");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,49 @@
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./index.css";
+import * as XLSX from"xlsx"
+
+
 function App() {
+  const [msg, setMsg] = useState();
+  const [status, setstatus] = useState(false);
+  const [emailList,setEmailList] = useState([])
 
-  const [msg,setMsg] = useState()
-  const [status,setstatus] = useState(false)
-
-  function handleMsg(event){
-    setMsg(event.target.value)
+  function handleMsg(event) {
+    setMsg(event.target.value);
   }
-  function send(){
-    setstatus(true)
-    axios.post("http://localhost:5000/sendmail",{msg:msg})
-    .then((data) => {
+
+  function handleFile(event) {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      const data = event.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetname = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetname];
+      const emailList = XLSX.utils.sheet_to_json(worksheet, { header: "A" });
+      const totalemail = emailList.map(function(item){
+        return item.a
+      })
+      console.log(totalemail)
+      setEmailList(totalemail)
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
+  function send() {
+    setstatus(true);
+    axios.post("http://localhost:5000/sendmail", { msg: msg, emailList:emailList}).then((data) => {
       if (data.data === true) {
-        alert("Email Sent successfully")
-        setstatus(false)
+        alert("Email Sent successfully");
+        setstatus(false);
       } else {
-        alert("Failed")
+        alert("Failed");
       }
-    })
+    });
   }
 
   return (
@@ -40,7 +64,8 @@ function App() {
 
       <div className="bg-[#3E87EA] flex flex-col items-center text-black px-5 py-3">
         <textarea
-          onChange={handleMsg} value={msg}
+          onChange={handleMsg}
+          value={msg}
           className="bg-white w-[80%] py-2 px-2 outline-none h-32 border border-black rounded-md"
           placeholder="Enter the Email text..."
         ></textarea>
@@ -48,14 +73,18 @@ function App() {
         <div>
           <input
             type="file"
+            onChange={handleFile}
             className="border-4 border-white border-dashed py-4 px-4 mt-5 mb-5"
           />
-          <p>Total Emails in the file : 0</p>
+          <p>Total Emails in the file : {emailList.length}</p>
 
-          <button onClick={send} className="mx-auto block text-blue-600 bg-white py-2 px-4 mt-2 font-medium border rounded-md hover:bg-blue-950 hover:border-none hover:text-white">
-            {status?"Sending...":"Send"}
+          <button
+            onClick={send}
+            className="mx-auto block text-blue-600 bg-white py-2 px-4 mt-2 font-medium border rounded-md hover:bg-blue-950 hover:border-none hover:text-white"
+          >
+            {status ? "Sending..." : "Send"}
           </button>
-        </div>        
+        </div>
       </div>
       <div className="bg-blue-300 text-white text-center p-18"></div>
       <div className="bg-blue-200 text-white text-center p-18"></div>
